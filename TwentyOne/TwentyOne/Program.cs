d8e36f49b608506fbcf2b880893a8a1080f2abbd
mpcahn <mpcahn@gmail.com>
2018-10-18 19:11:44 -0600

    Add timesShuffled

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -11,29 +11,36 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             Deck deck = new Deck();
-            deck = Shuffle(deck);
+            deck = Shuffle(deck, out int timesShuffled, 3);
             
             foreach (Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
             }
             Console.WriteLine(deck.Cards.Count);
+            Console.WriteLine("Times shuffled: {0}", timesShuffled);
             Console.ReadLine();
         }
 
-        public static Deck Shuffle(Deck deck)
+        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
         {
-            List<Card> TempList = new List<Card>();
-            Random rand = new Random();
-
-            while (deck.Cards.Count > 0)
+            timesShuffled = 0;
+            for (int i = 0; i < times; i++)
             {
-                int randI = rand.Next(0, deck.Cards.Count);
-                TempList.Add(deck.Cards[randI]);
-                deck.Cards.RemoveAt(randI);
+                timesShuffled++;                
+                List<Card> TempList = new List<Card>();
+                Random rand = new Random();
+
+                while (deck.Cards.Count > 0)
+                {
+                    int randI = rand.Next(0, deck.Cards.Count);
+                    TempList.Add(deck.Cards[randI]);
+                    deck.Cards.RemoveAt(randI);
+                }
+                deck.Cards = TempList;
+                
             }
-            deck.Cards = TempList;
             return deck;
-        }
+        }        
     }
 }
